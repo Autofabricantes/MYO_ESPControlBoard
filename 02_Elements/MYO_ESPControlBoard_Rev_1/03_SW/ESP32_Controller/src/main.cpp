bc95b1d6a7bfbb346d4cfe3c35d02c43deea854d
daytripper79 <lopezberbel@gmail.com>
2021-05-06 23:09:50 +0200

    Update

--- 02_Elements/MYO_ESPControlBoard_Rev_1/03_SW/ESP32_Controller/src/main.cpp
+++ 02_Elements/MYO_ESPControlBoard_Rev_1/03_SW/ESP32_Controller/src/main.cpp
@@ -1,132 +1,155 @@
-#include <Arduino.h>
-#include <WiFi.h>
+#include <myo.h>
 
-// My libraries
-#include "Constants.h"
-#include "InputOutputUtils.h"
-#include "Test.h"
+/**********************************************************************
+ * GFORCE EMG ARMBAND EXAMPLE
+ * Author: Davide Asnaghi
+ * Date: April 3rd 2019
+ * Requirements: 
+ *    - ESP32 arduino (https://github.com/espressif/arduino-esp32.git)
+ *    - ESP32 Myo library (https://github.com/project-sparthan/sparthan-myo.git)
+ **********************************************************************/
 
-#include "myo.h"
+armband myo;                // Myo BLE Armband
 
-// Conseguir verbose mode
+int emdData[16];
 
-int counter = 0;
-InputOutputUtils inputOutputUtils;
-Test tester;
+/********************************************************************************************************
+    SET CALLBACKS WHEN RECEIVING DATA
+ ********************************************************************************************************/
 
-myo myo;
+void batteryCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
 
-bool FirstTime = 0;
+  log_e(">> batteryCallback");
 
-void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  log_e("Battery: %i", pData[0]);
+  myo.battery = pData[0];
+  log_e("Battery: %d", myo.battery);
 }
 
-void notifyCallback0(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  int k;
-  for(k=0; k<=length; k++) {
-    log_e("Value [%i] : %i", k, pData[k]);
+void imuCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+
+  log_e(">> imuCallback");
+
+  log_e("IMU: \t");
+  for (int i = 0; i < length; i++) {
+    log_e("%d", pData[i]);
+  }
+  log_e("%d", millis());
+}
+
+void emgCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+
+  //log_e(">> emgCallback");
+  //log_e("EMG: \t");
+
+  for (int i = 0; i < length; i++) {
+    emdData[i] = pData[i];
+    //log_e("%d", pData[i]);
+  }
+  //log_e("%d", millis());
+}
+
+void gestureCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+  // Print the gesture
+  if (pData[0] == myohw_classifier_event_pose) {
+    log_e ("Gesture: ");
+    switch (pData[1]) {
+      case myohw_pose_rest:
+        log_e ("rest");
+        break;
+      case myohw_pose_fist:
+        log_e ("fist");
+        break;
+      case myohw_pose_wave_in:
+        log_e ("wave in");
+        break;
+      case myohw_pose_wave_out:
+        log_e ("wave out");
+        break;
+      case myohw_pose_fingers_spread:
+       log_e ("fingers spread");
+        break;
+      case myohw_pose_double_tap:
+        log_e ("double tap");
+        break;
+      default:
+        break;
+    }
   }
 }
 
-void setup() {
+/********************************************************************************************************
+    ARDUINO SKETCH
+ ********************************************************************************************************/
 
-  log_e(">> Setup.......");
-
-  Serial.begin(115200);
-
-
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-
-  // Start control board
-  inputOutputUtils.initIO();
-  if(mode == OPERATION_MODE || mode == TEST_MODE_TRANSITIONS)
-	  inputOutputUtils.initIO();
-  else if(mode ==  TEST_MODE_BOARD)
-    tester.setIoUtils(&inputOutputUtils);
-
-}
-
-// LOOP LLAMADA MYO
-void loop() {
-
-	counter++;
-
-  log_e("Into the loop %i", counter);
-
-  log_e("Execution mode :%i", mode);
-
-
-  if(mode == OPERATION_MODE || mode == TEST_MODE_TRANSITIONS)
-    inputOutputUtils.executeTransition();
-  else if(mode == TEST_MODE_BOARD)
-    tester.testingBoard();
-
-  // Testin MYO
-  // TODO: Imposible establecer la conexión bluetooth
-  // log_e("Conecting.....");
-  // myo.connect();
-
-  // log_e("Getting all data.....");
-  // myo.getAllData();
-
-  // if(!FirstTime) {
-  //   myo.pClient->getService(BLEUUID("0000180f-0000-1000-8000-00805f9b34fb"))->getCharacteristic(BLEUUID("00002a19-0000-1000-8000-00805f9b34fb"))->registerForNotify(notifyCallback); 
-  //   //myo.pClient->getService(BLEUUID("d5060005-a904-deb9-4748-2c7f4a124842"))->getCharacteristic(BLEUUID("d5060105-a904-deb9-4748-2c7f4a124842"))->registerForNotify(notifyCallback0); 
-  //   FirstTime = 1;     
-  // }  
+void setup()
+{
+  Serial.begin(115200);                             // Start serial interface for debugging
   
-  // log_e("Getting EMG notigfication.....");
-  // //myo.EMGNotify();
-
-  // log_e("Getting battery notigfication.....");
-  // myo.BATTNotify();
-  
-  
-  // if(myo.connected && myo.initDo) {
-
-  //   myo.getFirmwareVersion();
-
-  //   log_e("i", myo.fw_major);
-  //   log_e("i", myo.fw_minor);
-  //   log_e("i", myo.fw_patch);
-  //   log_e("i", myo.fw_hardware_rev);
-    
-  //   myo.initDo = false;
-  // }
-  
-
+  log_e ("Connecting...");
+  myo.connect();                                    // Connect to the myo
+  log_e ("Connected!!!!");
  
-  // if(myo.connected && myo.initDo) {
-  //   myo.getMyoInfo();
+  //delay(100);
+  vTaskDelay(100);
 
-  //   log_e("i", myo.fw_serial_number[0]);
-  //   log_e("i", myo.fw_serial_number[1]);
-  //   log_e("i", myo.fw_serial_number[2]);
-  //   log_e("i", myo.fw_serial_number[3]);
-  //   log_e("i", myo.fw_serial_number[4]);
-  //   log_e("i", myo.fw_serial_number[5]);
-  //   log_e("i", myo.fw_serial_number[6]);
-  //   log_e("i", myo.fw_unlock_pose);
-  //   log_e("i", myo.fw_active_classifier_type);
-  //   log_e("i", myo.fw_active_classifier_index);
-  //   log_e("i", myo.fw_has_custom_classifier);
-  //   log_e("i", myo.fw_stream_indicating);
-  //   log_e("i", myo.fw_sku);
-  //   log_e("i", myo.fw_reserved[0]);
-  //   log_e("i", myo.fw_reserved[1]);
-  //   log_e("i", myo.fw_reserved[2]);
-  //   log_e("i", myo.fw_reserved[3]);
-  //   log_e("i", myo.fw_reserved[4]);
-  //   log_e("i", myo.fw_reserved[5]);
-  //   log_e("i", myo.fw_reserved[6]);
-  //   log_e("i", myo.fw_reserved[7]);
-    
-  //   myo.initDo = false;
-  // }
-  
-  delay(1000);
-  
+  myo.set_sleep_mode(myohw_sleep_mode_never_sleep);
+
+  myo.set_myo_mode(myohw_emg_mode_send_emg,         // EMG mode
+                   myohw_imu_mode_send_data,        // IMU mode
+                   myohw_classifier_event_pose);    // Classifier mode
+
+  //myo.battery_notification(TURN_ON)->registerForNotify(batteryCallback);
+  myo.emg_notification(TURN_ON)->registerForNotify(emgCallback);
+  //myo.imu_notification(TURN_ON)->registerForNotify(imuCallback);
+  myo.gesture_notification(TURN_ON)->registerForNotify(gestureCallback);
+
+
+  // short vibration
+  myo.vibration(myohw_vibration_short);
+
+  myo.get_firmware();
+  log_e("Firmware version: %d.%d.%d.%d", myo.fw_major, myo.fw_minor, myo.fw_patch, myo.fw_hardware_rev);
+
+  log_e("Battery: %d", myo.battery);
+
+  myo.get_info();
+  log_e("Armband info: %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
+        myo.fw_serial_number[0], myo.fw_serial_number[1], myo.fw_serial_number[2], 
+        myo.fw_serial_number[3], myo.fw_serial_number[4], myo.fw_serial_number[5], 
+        myo.fw_serial_number[6], myo.fw_unlock_pose, myo.fw_active_classifier_type, 
+        myo.fw_active_classifier_index, myo.fw_has_custom_classifier, myo.fw_stream_indicating, 
+        myo.fw_sku, myo.fw_reserved[0], myo.fw_reserved[1], 
+        myo.fw_reserved[2], myo.fw_reserved[3], myo.fw_reserved[4], 
+        myo.fw_reserved[5], myo.fw_reserved[6], myo.fw_reserved[7]);
+
 }
 
+void loop()
+{  
+
+  log_e("EMG sample..................................................");
+
+  for (int i = 0; i < 16; i++) {
+     log_e("%d", emdData[i]);
+  }
+
+  // Detect disconnection
+  if (!myo.connected) {
+    log_e ("Device disconnected: reconnecting...");
+    myo.connect();
+    log_e ("Reconnected!!!!!");
+    myo.set_myo_mode(myohw_emg_mode_send_emg,               // EMG mode
+                     myohw_imu_mode_send_data,              // IMU mode
+                     myohw_classifier_mode_enabled);        // Classifier mode
+
+    //myo.battery_notification(TURN_ON)->registerForNotify(batteryCallback);
+    myo.emg_notification(TURN_ON)->registerForNotify(emgCallback);
+    //myo.imu_notification(TURN_ON)->registerForNotify(imuCallback);
+    myo.gesture_notification(TURN_ON)->registerForNotify(gestureCallback);
+
+  }
+  
+  vTaskDelay(1000);
+  //delay (5000);
+
+}
